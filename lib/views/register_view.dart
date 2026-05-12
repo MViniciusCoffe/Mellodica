eb9b41ed2dc89eb1b7ace9a7d216851b99800923
MViniciusCoffe <infobasicifrn2017marcusv@gmail.com>
2026-05-11 21:13:36 -0300

    feat: create basic Form UI

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import '../controllers/register_controller.dart';
 
 class RegisterView extends StatelessWidget {
   RegisterView({super.key});
+
+  final _controller = RegisterController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +14,33 @@ class RegisterView extends StatelessWidget {
         backgroundColor: Colors.amber,
       ),
 
-      body: Center(
-        child: Text("#Issue 1")
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Form(
+            key: _controller.formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _controller.emailController,
+                  decoration: const InputDecoration(labelText: "Email"),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _controller.passwordController,
+                  decoration: const InputDecoration(labelText: "Senha"),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () => _controller.submit(context),
+                  child: const Text("Registrar"),
+                )
+              ]
+            )
+          )
+        )
       )
     );
   }
