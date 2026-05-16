a25337f44bf8375222d576bb03b679f1800686a7
MViniciusCoffe <infobasicifrn2017marcusv@gmail.com>
2026-05-16 17:25:26 -0300

    refactor: move email and password validator to a separated class

--- lib/controllers/register_controller.dart
+++ lib/controllers/register_controller.dart
@@ -5,17 +5,8 @@ class RegisterController {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  String? validateEmail(String? value) {
-    if (value == null || value.isEmpty) return "Email é obrigatório";
-    final regex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-    return regex.hasMatch(value) ? null : "E-mail Inválido";
-  }
-
-  String? validatePassword(String? value) {
-    if (value == null || value.isEmpty) return "Senha é obrigatória";
-    if (value.length < 6) return "Senha deve ter pelo menos 6 caracteres";
-    return null;
-  }
+  String? validateEmail(String? value) => FieldsValidators().validateEmail(value);
+  String? validatePassword(String? value) => FieldsValidators().validatePassword(value);
 
   void submit(BuildContext context) {
     if (formKey.currentState?.validate() ?? false) {
@@ -33,5 +24,24 @@ class RegisterController {
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
+  }
+}
+
+class FieldsValidators {
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) return "Email é obrigatório";
+    final regex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return regex.hasMatch(value) ? null : "E-mail Inválido";
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) return "Senha é obrigatória";
+    if (value.length < 6) return "Senha deve ter pelo menos 6 caracteres";
+    return null;
+  }
+
+  static String? required(String? value, String fieldName) {
+    if (value == null || value.isEmpty) return "$fieldName é obrigatório";
+    return null;
   }
 }
